809e3a79e292cd09720ae16388bf28ae6766d2ba
gitfarag <react.codework@gmail.com>
2023-01-05 04:44:51 +0200

    test added

--- backend/src/spec/userEntity.spec.ts
+++ backend/src/spec/userEntity.spec.ts
@@ -2,7 +2,6 @@ import UsersEntity, { User } from "../entities/users.entity";
 
 const _User = new UsersEntity();
 const data: User = {
-  name: "ahmed",
   username: "admin",
   password: "secret",
 };
@@ -10,28 +9,22 @@ const data: User = {
 describe("users Class (create test user)", () => {
   it(`It should create user`, async () => {
     const result = await _User.createUser(data);
-    expect(result.name).toEqual("ahmed");
+    expect(result.username).toEqual("admin");
   });
   it(`It should get admin user`, async () => {
-    const result = await _User.getUserByUsername("admin");
-    expect(result.name).toEqual("ahmed");
+    const result = await _User.getUserByUsername(data.username);
+    expect(result.username).toEqual("admin");
   });
   it(`It should update  user`, async () => {
-    const users = await _User.index();
-    const uID = users[0].id;
+    const result = await _User.getUserByUsername(data.username);
+    result.username="john"
+    const updated = await _User.updateuser(result)
 
-    const result = await _User.updateuser({
-      id: uID,
-      username: "madison",
-      name: "Tester",
-      password: "password123",
-    });
-    expect(result.name).toEqual("Tester");
+    expect(updated.username).toEqual("john");
   });
   it(`It should delete  user`, async () => {
     const users = await _User.index();
     const id = (users[0].id as unknown) as number
-
     const result = await _User.deleteUser(users[0]);
     expect(result).toEqual(`User deleted with ID: ${id}`);
   });
